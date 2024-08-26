d554e3c63adeee9ad336f197d1f1412196a6e58e
ksks <nose2002kr@gmail.com>
2024-08-26 16:40:18 +0900

    Register AdMod ID

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 class AdHelper {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      if (kReleaseMode) {
+        return 'ca-app-pub-4133634045586474/3250054959';
+      } else {
+        return 'ca-app-pub-3940256099942544/6300978111';
+      }
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/2934735716';
     } else {
